35a664c0940c029693158fc19c1c6a6bd876d59c
SakshiPothikar <sakshipothikar028@gmail.com>
2024-02-09 19:20:26 +0530

    todo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use = (express.static(path.join(__dirname, "dist")))
+app.use(express.static(path.join(__dirname, "dist")))
 
 app.use("/api/admin", require("./routes/todoroutes"))
 app.use("*", (req, res) => {
